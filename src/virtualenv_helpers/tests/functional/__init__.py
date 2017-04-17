02fa55027accfce8dcc0c5fbee76023c77cbb9a1
David Pugh <djpugh@gmail.com>
2017-04-17 13:55:28 +0100

    Updating functional tests call

--- src/virtualenv_helpers/tests/functional/__init__.py
+++ src/virtualenv_helpers/tests/functional/__init__.py
@@ -1,7 +1,7 @@
 import unittest
 
-from virtualenv_helpers.tests.unit.test_activate import ActivateTestCase  # noqa F401
-from virtualenv_helpers.tests.unit.test_create import CreateTestCase  # noqa F401
+from virtualenv_helpers.tests.functional.test_activate import ActivateTestCase  # noqa F401
+from virtualenv_helpers.tests.functional.test_create import CreateTestCase  # noqa F401
 
 if __name__ == "__main__":
     unittest.main()
